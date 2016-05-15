88e977295b41eb9cf66c344628ddf0112db9b292
Alen Siljak <alen.siljak@yahoo.com>
2016-05-15 12:42:10 +0200

    painfully slow progress in adding a drawer

--- MMEx.Portable/MMEx.XForms.Droid/NativeMainActivity.cs
+++ MMEx.Portable/MMEx.XForms.Droid/NativeMainActivity.cs
@@ -8,6 +8,7 @@ using Android.Content;
 using Android.Content.PM;
 using Android.OS;
 using Android.Runtime;
+using Android.Support.V4.Widget;
 using Android.Views;
 using Android.Widget;
 
@@ -17,13 +18,17 @@ namespace MMEx.XForms.Droid
         ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class NativeMainActivity : Activity
     {
+        private DrawerLayout mDrawer;
+        //private MyActionBarDrawerToggle mDrawerToggle;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
-            ShowTutorial();
+            // todo ShowTutorial();
 
-            SetContentView(Resource.Layout.Main);
+            //SetContentView(Resource.Layout.Main);
+            CreateLayout();
 
             // todo set event handlers here.
         }
@@ -31,6 +36,37 @@ namespace MMEx.XForms.Droid
         private void ShowTutorial()
         {
             StartActivity(typeof(TutorialActivity));
+        }
+
+        private void CreateLayout()
+        {
+            SetContentView(Resource.Layout.Main);
+
+            // todo toolbar
+
+            // todo fragmentDetail
+
+            InitializeDrawer();
+        }
+
+        private void InitializeDrawer()
+        {
+            // navigation drawer
+            mDrawer = (DrawerLayout) FindViewById(Resource.Id.drawerLayout);
+
+            // set a custom shadow that overlays the main content when the drawer opens
+            if (mDrawer == null) return;
+
+            //todo mDrawerToggle = new MyActionBarDrawerToggle(this, mDrawer, R.string.open, R.string.closed);
+            //todo mDrawer.setDrawerListener(mDrawerToggle);
+
+            // create drawer menu
+            // todo initializeDrawerVariables();
+            // todo createExpandableDrawer();
+
+            // enable ActionBar app icon to behave as action to toggle nav drawer
+            // todo setDisplayHomeAsUpEnabled(true);
+            // todo getSupportActionBar().setDisplayShowTitleEnabled(true);
         }
     }
 }
